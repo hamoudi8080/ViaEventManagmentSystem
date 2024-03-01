caffba46a0dfc5f03f8c238c3774f4c9d3cbf589
hamoudi8080 <hamoderaso@outlook.com>
2024-03-01 13:01:22 +0100

    some folder created

--- src/Core/ViaEventManagmentSystem.Core.Domain/Aggregates/Guests/dummy.cs
+++ src/Core/ViaEventManagmentSystem.Core.Domain/Aggregates/Guests/dummy.cs
@@ -1,6 +1,7 @@
 ﻿namespace ViaEventManagmentSystem.Core.Domain.Aggregates.Guests;
 
 public class dummy
-{
-    
+{   //Create a “Common” directory. Here we put stuff which may be shared across the Domain project,
+    //and may not otherwise have an obvious location.
+    //We need base classes for AggregateRoot, Entity, and ValueObject, and maybe others.
 }
---
+++ src/Core/ViaEventManagmentSystem.Core.Domain/Common/Values/dummy.cs
@@ -0,0 +1,6 @@
+﻿namespace ViaEventManagmentSystem.Core.Domain.Common.Values;
+
+public class dummy
+{
+    //Common Values You may have value objects which do not belong to a specific aggregate, so they can be put in Common/Values.
+}
