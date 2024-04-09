ebcfc4578203faac2d144c7934610420aeac8714
hamoudi8080 <hamoderaso@outlook.com>
2024-04-09 08:26:06 +0200

    something in assignment 6 is done

---
+++ src/Core/ViaEventManagmentSystem.Core.AppEntry/Dispatcher/ICommandDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using ViaEventManagmentSystem.Core.AppEntry.Commands;
+
+namespace ViaEventManagmentSystem.Core.AppEntry.Dispatcher;
+
+public interface ICommandDispatcher
+{
+    Task<Result> DispatchAsync<TCommand>(TCommand command) where TCommand : ICommand;
+}
